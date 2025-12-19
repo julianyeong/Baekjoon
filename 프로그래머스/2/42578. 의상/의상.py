b4b2578260c33396df16da9b8cafd5db0908f8fd
julianyeong <111331805+julianyeong@users.noreply.github.com>
2025-12-19 17:43:54 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,13 +1,28 @@
+# def solution(clothes):
+#     answer = 1
+#     #1. 의상종류로 hashmap 만들기, value += 1
+#     hashmap = {}
+#     for i in range(len(clothes)):
+#         hashmap[clothes[i][1]] = 1
+#     for j in range(len(clothes)):
+#         hashmap[clothes[j][1]] += 1
+        
+#     #2. return value 값들의 곱 -1 
+#     for z in hashmap:
+#         answer *= hashmap[z]
+#     return answer-1
+
+#간결ver
 def solution(clothes):
     answer = 1
-    #1. 의상종류로 hashmap 만들기, value += 1
-    hashmap = {}
-    for i in range(len(clothes)):
-        hashmap[clothes[i][1]] = 1
-    for j in range(len(clothes)):
-        hashmap[clothes[j][1]] += 1
-        
-    #2. return value 값들의 곱 -1 
-    for z in hashmap:
-        answer *= hashmap[z]
+    
+    types = {}
+    for c, t in clothes:
+        if t not in types:
+            types[t] = 2
+        else:
+            types[t] += 1
+            
+    for cnt in types.values():
+        answer *= cnt
     return answer-1
